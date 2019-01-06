db46e523518c6e8d2f53b2dab9f955e16a9cd700
Ryoh Kawai <kawairyoh@gmail.com>
2019-01-06 21:50:33 +0900

    Separate lookup/output process

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,47 @@ struct Cli {
     ipaddr: String,
 }
 
+struct OutputData {
+    iso_code: String,
+    name: String,
+}
+
+impl OutputData {
+    fn new(iso_code: &str, name: &str) -> OutputData {
+        OutputData {
+            iso_code: iso_code.to_string(),
+            name: name.to_string(),
+        }
+    }
+
+    fn print(&self, handle: &mut io::BufWriter<io::StdoutLock>) {
+        writeln!(
+            handle,
+            "GeoIP Country Edition: {}, {}",
+            self.iso_code, self.name
+        )
+        .unwrap();
+    }
+}
+
+fn geoiplookup(reader: &maxminddb::Reader<Vec<u8>>, address: &IpAddr) -> OutputData {
+    // Lookup GeoIP data
+    let city: geoip2::City = reader.lookup(*address).expect("none");
+    //println!("{:#?}", city);
+
+    // Country base
+    let country = city.country.expect("Get error country variable");
+
+    // ISO Country code
+    let iso_code = country.iso_code.unwrap_or("O1".to_string());
+
+    // Country name
+    let names = country.names.unwrap();
+    let name = names.get("en").unwrap();
+
+    OutputData::new(&iso_code, &name)
+}
+
 fn main() -> CliResult {
     let stdout = io::stdout();
     let mut handle = io::BufWriter::new(stdout.lock());
@@ -28,22 +69,11 @@ fn main() -> CliResult {
     let mmdb = "GeoLite2-City.mmdb";
     let reader = Reader::open_readfile(mmdb).expect("Open error");
 
-    // Loopup Geoip data
-    let city: geoip2::City = reader.lookup(ipaddr).expect("Lookup error");
-    //println!("{:#?}", city);
-    let country = city.country.expect("Get error country variable");
+    // Get output data
+    let data = geoiplookup(&reader, &ipaddr);
 
-    // ISO Country code
-    let iso_code = country.iso_code.unwrap_or("O1".to_string());
-
-    // Country name
-    let names = country.names.unwrap();
-    let name = names.get("en").unwrap().to_string();
-
-    // City code
-    let _continet = city.continent.expect("Get error continent variable");
-
-    writeln!(handle, "GeoIP Country Edition: {}, {}", iso_code, name).unwrap();
+    // print
+    data.print(&mut handle);
 
     Ok(())
 }
