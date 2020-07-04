98cec012676484a9f78f8ad2b75a7197a0f58a60
Ryoh Kawai <kawairyoh@gmail.com>
2020-07-05 02:03:27 +0900

    Add sample program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use maxminddb::geoip2;
+use std::net::IpAddr;
+use std::str::FromStr;
+
+fn main() -> anyhow::Result<()> {
+    // test parameters
+    let mmdb_path = "/var/lib/GeoIP/GeoLite2-City.mmdb";
+    let test_ipaddr = "52.25.53.18";
+
+    // open geoip database
+    let reader = maxminddb::Reader::open_readfile(&mmdb_path)?;
+
+    // lookup geoip data
+    let ipaddr: IpAddr = FromStr::from_str(test_ipaddr)?;
+    let city: geoip2::City = reader.lookup(ipaddr)?;
+
+    println!("{:#?}", city.continent);
+
+    Ok(())
 }
