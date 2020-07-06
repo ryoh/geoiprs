de7f9dbd0ba48a7662dfb03ea7ff22d9e80c4339
Ryoh Kawai <kawairyoh@gmail.com>
2020-07-07 01:19:59 +0900

    Change IPアドレスを引数に国コードを返却できるようにした。

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
 use maxminddb::geoip2;
 use std::net::IpAddr;
-use std::str::FromStr;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    ipaddr: IpAddr,
+}
 
 fn main() -> anyhow::Result<()> {
-    // test parameters
-    let mmdb_path = "/var/lib/GeoIP/GeoLite2-City.mmdb";
-    let test_ipaddr = "52.25.53.18";
+    // get parameters
+    let opt = Opt::from_args();
 
     // open geoip database
+    let mmdb_path = "/var/lib/GeoIP/GeoLite2-City.mmdb";
     let reader = maxminddb::Reader::open_readfile(&mmdb_path)?;
 
     // lookup geoip data
-    let ipaddr: IpAddr = FromStr::from_str(test_ipaddr)?;
-    let city: geoip2::City = reader.lookup(ipaddr)?;
+    let city: geoip2::City = reader.lookup(opt.ipaddr)?;
+    println!("City: {:?}", city.city.unwrap().names.unwrap()["ja"]);
 
-    println!("{:#?}", city.continent);
+    let country = city.country.unwrap();
+    println!("ISO Code: {:?}", country.iso_code.unwrap());
+
+    let continent = city.continent.unwrap();
+    println!("Continent Code: {:?}", continent.code.unwrap());
 
     Ok(())
 }
